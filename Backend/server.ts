4e867f0827a830ecd7693500c5be74fcfef0dab2
Ishrz <ishrzz66@gmail.com>
2026-07-05 00:14:50 +0530

    port

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,9 +1,9 @@
 import config from "./src/config/config.js"
 import app from "./src/app.js"
 
-
+const PORT = process.env.PORT || 10000
 
 
 app.listen(3000, ()=>{
-    console.log("Server is connected to port : ", 3000)
+    console.log("Server is connected to port : ", PORT)
 })
